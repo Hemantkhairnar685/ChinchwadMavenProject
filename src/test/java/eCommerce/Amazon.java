dd0722b377874f820c21471232d0ed17a0ae5e05
Hemantkhairnar685 <hemantkhairnar685@gmail.com>
2023-07-27 20:52:24 +0530

    [Hemant] Adding my first code

---
+++ src/test/java/eCommerce/Amazon.java
@@ -0,0 +1,21 @@
+package eCommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Amazon {
+  @Test
+  
+	  public void amazontest() throws InterruptedException 
+	  {
+		  WebDriver driver = new ChromeDriver();
+		  Reporter.log("opening amazon", true);
+		  driver.get("https://www.amazon.in/");
+		  Thread.sleep(7000);
+		  driver.close();
+	  }
+	
+  }
+
---
+++ src/test/java/eCommerce/Flipkart.java
@@ -0,0 +1,20 @@
+package eCommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Flipkart {
+  @Test
+  
+	  public void flipkarttest() throws InterruptedException {
+		  WebDriver driver = new ChromeDriver();
+		  Reporter.log("opening Flipkart", true);
+		  driver.get("https://www.flipkart.com/");
+		  Thread.sleep(7000);
+		  driver.close();
+	  }
+	
+  }
+
